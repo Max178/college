e77203cbb40e08f9f5598e30fb53abd19c1eb9bf
Max178 <maxpendse@gmail.com>
2022-07-15 11:16:19 -0400

    git commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import React, {useState} from "react";
 import Schools from "./schools.json";
 
+//Define Score Values
 var SATVal = 0
 var GPAScore = 0
 var ECScore = 0
@@ -11,14 +12,13 @@ var ServiceScore = 0
 export default function App() {
 
   //Define States i.e. the values of the numbers
-  const [satScore, setSatScore] = useState("")
-  const [gpaAverage, setGpaAverage] = useState("")
-  const [schoolSelection, setSchoolSelection] = useState("North Carolina")
+  const [satScore, setSatScore] = useState("0")
+  const [gpaAverage, setGpaAverage] = useState("0")
+  const [schoolSelection, setSchoolSelection] = useState("None")
   const [extraCurricular, setExtraCurricular] = useState("0")
   const [summerHours, setSummerHours] = useState("0")
   const [communityServiceHours, setCommunityServiceHours] = useState("0")
 
-  //Define Score Values
 
 
   //Score Calculating Functions
@@ -43,7 +43,7 @@ export default function App() {
   function calculateSummerScore(e) {
     setSummerHours(e)
     const pd = parseInt(e)
-    console.log(pd)
+    console.log("hi")
     if (pd) {
       if (pd > 3) {
         SummerScore = 5
@@ -51,16 +51,17 @@ export default function App() {
       if (pd <= 3 && pd > 2) {
         SummerScore = 4
       }
-      if (pd > 0) {
+      if (pd > 0 && pd <= 2) {
         SummerScore = 2
       }
     }
+    console.log(SummerScore)
   }
 
   function calculateServiceScore(e) {
     setCommunityServiceHours(e)
     const pd = parseInt(e)
-    console.log(pd)
+    
     if (pd) {
       if (pd > 3) {
         ServiceScore = 5
@@ -68,36 +69,10 @@ export default function App() {
       if (pd <= 3 && pd > 2) {
         ServiceScore = 4
       }
-      if (pd > 0) {
+      if (pd > 0 && pd <= 2) {
         ServiceScore = 2
       }
     }
-    console.log(ServiceScore)
-  }
-
-  
-
-
-  const [finalText, setFinalText] = useState("Not Applicable Account")
-
-  const SchoolNameArr = Schools.map((schools) => {return (schools.name)})
-
-  function schoolChange (e, sat, gpa) {
-    console.log(ECScore)
-    const iSat = parseInt(sat)
-    const iGPA = parseInt(gpa)
-    const iECs = parseInt(extraCurricular)
-    const iSh = parseInt(summerHours)
-    const iCS = parseInt(communityServiceHours)
-    if (iSat && iGPA && iECs && iSh && iCS && SchoolNameArr.includes(e)) {
-      var dr = ServiceScore + SummerScore + ECScore
-      setFinalText("We Estimate your score to be " + dr.toString())
-      console.log(SchoolNameArr.indexOf(e))
-    }
-    else {
-      setFinalText("Not Availibe School. Please Enter All Valid INformation")
-    }
-    setSchoolSelection(e)
   }
 
   const School = Schools.map(
@@ -114,8 +89,41 @@ export default function App() {
     }
   )
 
+
+  const [finalText, setFinalText] = useState("Not Applicable Account")
+
+  const SchoolNameArr = Schools.map((schools) => {return (schools.name)})
+
+  function schoolChange (e, sat, gpa) {
+    console.log(ECScore)
+    const iSat = parseInt(sat)
+    const iGPA = parseInt(gpa)
+    const iECs = parseInt(extraCurricular)
+    const iSh = parseInt(summerHours)
+    const iCS = parseInt(communityServiceHours)
+    if (iSat && iGPA && iECs && iSh && iCS && SchoolNameArr.includes(e)) {
+      const fin = Schools[SchoolNameArr.indexOf(e)]
+      //var dr = (iSat - fin.satUpper) * fin.weights[0] + ECScore * fin[2] + SummerScore * fin[3] + ServiceScore * fin[4]
+      var dr = (iSat - fin.satUpper) * fin.weights[0] + ECScore * fin.weights[2] + SummerScore * fin.weights[3] + ServiceScore * fin.weights[4]
+      console.log(dr)
+      setFinalText("We Estimate your score to be " + dr.toString())
+      
+      
+    }
+    else {
+      setFinalText("Not Availibe School. Please Enter All Valid Information")
+    }
+    setSchoolSelection(e)
+  }
+
+
+
   return(
     <>
+    <div className = 'head'>
+      <h1>Akala: Chance-Me Feature</h1>
+    </div>
+    <div className = 'inputs'>
     <p>Please Enter Your SAT Score</p>
     <input type = "text" name = "name" onChange = {(e) => setSatScore(e.target.value)} value = {satScore}/>
     <p>Please Enter Your GPA</p>
@@ -126,9 +134,13 @@ export default function App() {
     <input type = "text" name = "name" onChange = {(e) => calculateServiceScore(e.target.value)} value = {communityServiceHours}/>
     <p>Please Enter Your Summer Experience Hours</p>
     <input type = "text" name = "name" onChange = {(e) => calculateSummerScore(e.target.value)} value = {summerHours}/>
+    </div>
     
+    <div className = 'collegeSelect'>
     <p>Please Enter The College you want</p>
     <input type = "text" name = "name" onChange = {(e) => schoolChange(e.target.value, satScore, gpaAverage)} value = {schoolSelection}/>
+    </div>
+    
     
     
     <p>{finalText}</p>
